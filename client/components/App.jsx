6eb5345b60b2a646130a0b58be17f2fd5c25a2f9
Martin Lekvall <martin.lekvall@hd.se>
2017-02-18 02:48:33 +0100

    Add React and confirm working

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div style={{display: 'flex'}}>
+        <h1>Yolo</h1>
+      </div>
+    )
+  }
+}
