f70f61efcf87d988ce013984eb0c028a2904dfd3
Martin Lekvall <martin.lekvall@hd.se>
2017-02-18 03:40:15 +0100

    Use eslint (semistandard, react)

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export default class App extends React.Component {
-  render() {
+  render () {
     return (
       <div style={{display: 'flex'}}>
         <h1>Yolo</h1>
       </div>
-    )
+    );
   }
 }
