685b33d8ab5f0b2b54bb09c5c740c4234b00b988
Martin Lekvall <lekvall@gmail.com>
2017-11-07 00:21:17 +0100

    Add sentry logging

--- sources/js/index.jsx
+++ sources/js/index.jsx
@@ -4,4 +4,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var App = require('./components/App.jsx');
 
+Raven.config('https://a04fec6b6a794786bcdfbc90b52a43b9@sentry.io/240834').install();
+
 ReactDOM.render(<App />, document.getElementById('root'));
